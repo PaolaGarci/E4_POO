fe12917e3c24164d853074bdc96cca60488bdf65
ernestogm812 <ernesto.gm812@gmail.com>
2021-04-24 16:17:32 -0500

    Inventario

---
+++ inventario_productos.cpp
@@ -0,0 +1,13 @@
+//
+//  inventario_productos.cpp
+//  invernario_productos
+//
+//  Created by Ernesto Godinez on 24/04/21.
+//
+
+#include "inventario_productos.hpp"
+//CONSTRUCTORES
+Inventario_productos::Inventario_productos(){
+    cantidad=10;
+}
+

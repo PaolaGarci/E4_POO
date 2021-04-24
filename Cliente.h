e384d86f2010953230119978c360b9db8d2666f1
VaneA01634064 <83040616+VaneA01634064@users.noreply.github.com>
2021-04-24 16:34:49 -0500

    Update Cliente.h

--- Cliente.h
+++ Cliente.h
@@ -1,3 +1,16 @@
-#include "Cliente.h"
-df
+#ifndef CLIENTE_H_INCLUDED
+#define CLIENTE_H_INCLUDED
+#include "Persona.h"
 
+using namespace std;
+
+class Cliente:public Persona{
+private:
+    int RFC;
+
+public:
+    Cliente();
+    Cliente( int rfc);
+
+};
+#endif // PAQUETE_H_INCLUDED

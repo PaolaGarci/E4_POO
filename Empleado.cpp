79a6fc2ea7727ba224b4fb5914fdb4a7bccb0304
VaneA01634064 <83040616+VaneA01634064@users.noreply.github.com>
2021-04-28 14:57:28 -0500

    Update Empleado.cpp

---
+++ Empleado.cpp
@@ -0,0 +1,17 @@
+#ifndef EMPLEADO_H_INCLUDED
+#define EMPLEADO_H_INCLUDED
+#include "Persona.h"
+
+using namespace std;
+
+class Empleado:public Persona{
+private:
+    string Cargo;
+    string Ubicacion;
+  
+public:
+    Cargo();
+    Cargo( string car, string ubi);
+   
+};
+#endif // EMPLEADO_H_INCLUDED

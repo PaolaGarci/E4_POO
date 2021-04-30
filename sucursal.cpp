068c98731207db20865ea1c20b72af38bfee61a1
ernestogm812 <ernesto.gm812@gmail.com>
2021-04-30 11:00:23 -0500

    creación 1 de clase sucursal

--- sucursal.cpp
+++ sucursal.cpp
@@ -6,3 +6,25 @@
 //
 
 #include "sucursal.hpp"
+Sucursal::Sucursal(){
+    Producto P1(),P2(),P3(),P4(),P5(); //Crea productos default para la lista, esta tendrá q ser diferente al decidir q productos queremos
+    listaProductos={P1,P2,P3,P4,P5};
+    nombre=" ";
+    string listaSucursales={"GDL","MONT","CDMX","QRO","AGS","SANP","VALLARTA","CANCUN","HIDALGO","PUEBLA"};
+}
+Sucursal::Sucursal(string nom){
+    Producto P1(),P2(),P3(),P4(),P5(); //Crea productos default para la lista, esta tendrá q ser diferente al decidir q productos queremos
+    listaProductos={P1,P2,P3,P4,P5};
+    nombre=nom;
+    
+}
+//metodos q a/m/b productos en la lista
+void Sucursal::agregar(Producto p){
+    
+}
+void Sucursal::modificar(Producto p){
+    
+}
+void Sucursal::borrar(){
+    
+}
--- sucursal.hpp
+++ sucursal.hpp
@@ -7,7 +7,22 @@
 
 #ifndef sucursal_hpp
 #define sucursal_hpp
-
 #include <stdio.h>
+#include "Producto.hpp"
+#include <iostream>
+using namespace std;
+class Sucursal{
+    public:
+        Sucursal();
+        Sucursal(string);
+        void agregar(Producto);//tiene q haber productos adentro de los metodos segun yo
+        void modificar(Producto);
+        void borrar();
+    protected:
+        string nombre;
+        Producto listaProductos[5];
+        string listaSucursales[10];
+
+};
 
 #endif /* sucursal_hpp */

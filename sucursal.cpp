5d3f526a537e51a6ed43b0459ed682a4c4686754
ernestogm812 <ernesto.gm812@gmail.com>
2021-04-30 11:39:22 -0500

    modificación a sucursal 2

--- sucursal.cpp
+++ sucursal.cpp
@@ -5,16 +5,18 @@
 //  Created by Ernesto Godinez on 28/04/21.
 //
 
-#include "sucursal.hpp"
+#include "Sucursal.hpp"
 Sucursal::Sucursal(){
-    Producto P1(),P2(),P3(),P4(),P5(); //Crea productos default para la lista, esta tendrá q ser diferente al decidir q productos queremos
-    listaProductos={P1,P2,P3,P4,P5};
+    Producto P1,P2,P3,P4,P5 ; //Crea productos default para la lista, esta tendrá q ser diferente al decidir q productos queremos
+    Producto listaP[5]={P1,P2,P3,P4,P5};
+    listaProductos=listaP;
     nombre=" ";
-    string listaSucursales={"GDL","MONT","CDMX","QRO","AGS","SANP","VALLARTA","CANCUN","HIDALGO","PUEBLA"};
+    string listaSucursales[10]={"GDL","MONT","CDMX","QRO","AGS","SANP","VALLARTA","CANCUN","HIDALGO","PUEBLA"};
 }
 Sucursal::Sucursal(string nom){
-    Producto P1(),P2(),P3(),P4(),P5(); //Crea productos default para la lista, esta tendrá q ser diferente al decidir q productos queremos
-    listaProductos={P1,P2,P3,P4,P5};
+    Producto P1,P2,P3,P4,P5; //Crea productos default para la lista, esta tendrá q ser diferente al decidir q productos queremos
+    Producto listaP[5]={P1,P2,P3,P4,P5};
+    listaProductos=listaP;
     nombre=nom;
     
 }
@@ -27,4 +29,9 @@ void Sucursal::modificar(Producto p){
 }
 void Sucursal::borrar(){
     
+}
+void Sucursal::imprimirSucursales(){
+    for (int i=0; i<10; i++){
+        cout << listaSucursales[i] <<endl;
+    }
 }
